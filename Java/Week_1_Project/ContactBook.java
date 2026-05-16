4fba7f17622776dc3d76f6b0d13f78fa6093efab
SomeGuyWithASombrero <135500489+justsomeguywithasombrero@users.noreply.github.com>
2026-05-16 10:56:19 -0400

    Project Files

--- Java/Week_1_Project/ContactBook.java
+++ Java/Week_1_Project/ContactBook.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
 
 
 public class ContactBook 
@@ -9,5 +10,21 @@ public class ContactBook
         //Object Initialisation
         Scanner scan = new Scanner(System.in);
         ArrayList<Contact> contactList = new ArrayList<Contact>();
+        Contact contact = new Contact();
+
+        System.out.print("Enter the name of your new Contact: ");
+        String name = scan.nextLine();
+        System.out.print("Enter the number of your new Contact: ");
+        String number = scan.nextLine();
+
+        //Populating the  object
+        contact.setName(name);
+        contact.setNumber(number);
+
+        //Populating the arrayList
+        contactList.add(contact);
+
+        
+
     }
 }
