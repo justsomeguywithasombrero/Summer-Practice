8a41846caab90dfecd851ec06d6336dd0a28946a
SomeGuyWithASombrero <135500489+justsomeguywithasombrero@users.noreply.github.com>
2026-05-16 10:45:06 -0400

    Project Files: Contact and ContactBook

---
+++ Java/Week_1_Project/Contact.java
@@ -0,0 +1,30 @@
+public class Contact
+{
+    private String Name;
+    private String Number;
+    public Contact()
+    {
+        Name = "Default";
+        Number = "000";
+    }
+
+    //Muatators
+    public void setName(String newName)
+    {
+        this.Name = newName;
+    }
+    public void setNumber(String newNumber)
+    {
+        this.Name = newNumber;
+    }
+
+    //Acessors
+    public String getName()
+    {
+        return this.Name;
+    }
+    public String getNumber()
+    {
+        return this.Number;
+    }
+}
---
+++ Java/Week_1_Project/ContactBook.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class ContactBook 
+{
+    public static void main(String[] args)
+    {
+        //Object Initialisation
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Contact> contactList = new ArrayList<Contact>();
+    }
+}
